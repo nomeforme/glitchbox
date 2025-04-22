7a4f590773225d3326d91cd1ba54e2c45f842f87
yaoe <pdefilippi@gmail.com>
2025-04-22 15:52:07 +0200

    output bg removal

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,5 @@
 # export HF_HOME="/media/monsterdrive/models"
-uv run server/main.py --pipeline controlnetSDTurbot2i # --sfast
+uv run server/main.py --pipeline controlnetSDTurbot2i --taesd # --torch_compile # --sfast
 # uv run server/main.py --pipeline img2img
 # uv run server/main.py --pipeline img2imgStreamDiffusion --taesd --tensorrt # --sfast
 # uv run server/main.py --pipeline controlnetSDTurbo
